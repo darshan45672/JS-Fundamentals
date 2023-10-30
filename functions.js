783ee144ab1aa174b786c232c11406a58f38e341
darshan45672 <drshnbhandary@gmail.com>
2023-10-30 11:31:49 +0530

    added files

---
+++ functions.js
@@ -0,0 +1,12 @@
+function add(x, y) {
+    return console.log("Sum is " + (x+y))
+}
+
+const sub = (x, y)=> {
+    return console.log("difference is " + (x-y))
+}
+
+for (let i = 0; i < 10; i++) {
+    add(i, i+1);
+    sub(i, i+1)
+}
--- looping.js
+++ looping.js
@@ -2,3 +2,35 @@ for (let i = 0; i < 500; i++) {
     console.log(i);
     
 }
+
+let obj = {
+    nidhi : 5,
+    darshan : 10,
+    nithesh : 20,
+    gaurav : 30,
+}
+
+// usign for loop for objects
+for (let a in obj) {
+    console.log(a + obj[a])
+}
+
+// for of loop
+for (const b of "darshan") {
+    console.log(b)
+}
+
+// while loop
+let i = 0;
+let n = 100;
+while (i<n) {
+    console.log(i)
+    i++
+}
+
+
+// do while loop
+do {
+    console.log(i)
+    i++
+} while (i<n);
